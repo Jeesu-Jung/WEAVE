95be266c18c45ac4e3614c56e94d2f53616454c4
potionk <jongunkim.p@gmail.com>
2025-10-30 00:16:14 +0900

    feat: feign client timeout 변경

--- grove-cache-service/src/main/kotlin/io/jeesu/grovecacheservice/infrastructure/config/FeignConfig.kt
+++ grove-cache-service/src/main/kotlin/io/jeesu/grovecacheservice/infrastructure/config/FeignConfig.kt
@@ -2,13 +2,17 @@ package io.jeesu.grovecacheservice.infrastructure.config
 
 import feign.Logger
 import feign.Request
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class FeignConfig {
+class FeignConfig(
+    @Value("\${feign.client.config.default.connectTimeout:15000}") private val connectTimeoutMs: Int,
+    @Value("\${feign.client.config.default.readTimeout:15000}") private val readTimeoutMs: Int
+) {
     @Bean
-    fun feignOptions(): Request.Options = Request.Options(15_000, 15_000)
+    fun feignOptions(): Request.Options = Request.Options(connectTimeoutMs, readTimeoutMs)
 
     @Bean
     fun feignLoggerLevel(): Logger.Level = Logger.Level.BASIC
